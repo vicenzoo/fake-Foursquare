908a63c6eed908a208cd9da4a8a2f3cef858aa9a
vicenzoo <59846597+vicenzoo@users.noreply.github.com>
2022-12-11 22:20:11 -0300

    Add files via upload

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Computacao\Desktop\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Computacao\Documents\flutter\flutter_application"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Proprietario\Downloads\flutter-doglfe-master\flutterd"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
